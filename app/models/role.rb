56b7b36e00f8f26bfd7aed62f201ae4e2e97789b
Martin Souto <msouto2001@gmail.com>
2021-12-05 14:13:10 -0300

    Creo el seeds.rb

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,11 @@ class Role < ApplicationRecord
     has_and_belongs_to_many :users
 
     validates :name, presence: true, uniqueness: true
+
+    before_destroy do | role |
+        if role.users.count > 0
+            role.errors.add( :base, "No se puede eliminar un rol si tiene usuarios asignados")
+            throw :abort
+        end
+    end
 end
