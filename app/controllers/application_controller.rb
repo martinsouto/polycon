9c05690390eb0b2de0f9f458139addcdd2f608d4
Martin Souto <msouto2001@gmail.com>
2021-12-05 00:38:50 -0300

    Agrego protect from forgery prepend true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery prepend: true
+
+    def has_permission(permi)
+        begin
+            if ! current_user.roles.all.any? { |rol| rol.permissions.all.any? { |per| per.name == permi} }
+                flash[:alert] = "#{current_user.roles.first.permissions.first.name} No tienes permiso para acceder a esa página"
+                redirect_to home_show_path
+            end
+        rescue
+            flash[:alert] = "#{current_user.email} no tienes permiso para acceder a esa página"
+            redirect_to home_show_path
+        end
+    end
 end
