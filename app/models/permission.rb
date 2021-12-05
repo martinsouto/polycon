c0192573b9cfd806c3bf79651bc466ea70b928bf
Martin Souto <msouto2001@gmail.com>
2021-12-05 00:01:30 -0300

    Creo roles y permisos, especificando las relaciones muchos a muchos entre ellos

---
+++ app/models/permission.rb
@@ -0,0 +1,6 @@
+class Permission < ApplicationRecord
+
+    has_and_belongs_to_many :roles
+
+    validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_004221) do
+ActiveRecord::Schema.define(version: 2021_12_05_012303) do
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_permissions_on_name", unique: true
+  end
+
+  create_table "permissions_roles", id: false, force: :cascade do |t|
+    t.integer "permission_id", null: false
+    t.integer "role_id", null: false
+    t.index ["permission_id"], name: "index_permissions_roles_on_permission_id"
+    t.index ["role_id"], name: "index_permissions_roles_on_role_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_roles_on_name", unique: true
+  end
+
+  create_table "roles_users", id: false, force: :cascade do |t|
+    t.integer "role_id", null: false
+    t.integer "user_id", null: false
+    t.index ["role_id"], name: "index_roles_users_on_role_id"
+    t.index ["user_id"], name: "index_roles_users_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
