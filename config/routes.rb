6ed3e8aaf7b7bf7793597bce9766711f4b37b6e8
Martin Souto <msouto2001@gmail.com>
2021-12-05 03:25:21 -0300

    CRUD de usuarios casi terminado, falta asignarles el rol

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#show'
   get 'home/show'
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
+  resources :users
 end
